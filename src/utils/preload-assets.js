60943df7ba5aca7bf5fac86e4a90fe728fb73967
Kristo <christo.losier@gmail.com>
2026-06-21 12:39:56 +0200

    fix(assets): preload active collection frame

--- src/utils/preload-assets.js
+++ src/utils/preload-assets.js
@@ -2,7 +2,7 @@ import { assetUrl } from "./asset-url.js";
 import { porterStoutCards, porterStoutCollection } from "../data/porters-stouts-collection.js";
 
 export const CARD_BACK_URL = assetUrl(porterStoutCollection.cardBack);
-export const CARD_FRAME_URL = assetUrl("/assets/collections/porters-stouts/card-front-frame.svg");
+export const CARD_FRAME_URL = assetUrl(porterStoutCollection.cardFrame);
 
 export async function preloadAssets(onProgress) {
   const urls = [
@@ -16,17 +16,10 @@ export async function preloadAssets(onProgress) {
 
   const load = (url) =>
     new Promise((resolve) => {
-      const isSvg = url.endsWith(".svg");
-      if (isSvg) {
-        fetch(url)
-          .then(() => { loaded++; onProgress && onProgress(loaded / total); resolve(); })
-          .catch(() => { loaded++; onProgress && onProgress(loaded / total); resolve(); });
-      } else {
-        const img = new Image();
-        img.onload = () => { loaded++; onProgress && onProgress(loaded / total); resolve(); };
-        img.onerror = () => { loaded++; onProgress && onProgress(loaded / total); resolve(); };
-        img.src = url;
-      }
+      const img = new Image();
+      img.onload = () => { loaded++; onProgress && onProgress(loaded / total); resolve(); };
+      img.onerror = () => { loaded++; onProgress && onProgress(loaded / total); resolve(); };
+      img.src = url;
     });
 
   await Promise.all(urls.map(load));
