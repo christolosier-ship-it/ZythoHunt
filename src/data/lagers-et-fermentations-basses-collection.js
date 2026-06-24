b22097ee2cfc6474c74e17c89fe7f46cdadbacb3
Kristo <christo.losier@gmail.com>
2026-06-24 22:48:48 +0200

    Integrate lager Brassopedie collection

--- src/data/lagers-et-fermentations-basses-collection.js
+++ src/data/lagers-et-fermentations-basses-collection.js
@@ -1,8 +1,8 @@
-import collectionJson from "../../docs/active/collection-01-lagers-et-fermentations-basses.json" with { type: "json" };
+import collectionJson from "./brassopedie/collection-01-lagers-et-fermentations-basses.json" with { type: "json" };
 import { createCollectionBundle } from "./create-collection-bundle.js";
 import { lagersEtFermentationsBassesAssets, lagersEtFermentationsBassesAssetPath, lagersEtFermentationsBassesCardImages, lagersEtFermentationsBassesThumbPath } from "./card-assets/lagers-et-fermentations-basses-assets.js";
 
-export const lagersEtFermentationsBassesBundle = createCollectionBundle({
+const lagersEtFermentationsBassesBundleSource = createCollectionBundle({
   collectionJson,
   collectionId: "lagers-et-fermentations-basses",
   slug: "lagers-et-fermentations-basses",
@@ -22,3 +22,10 @@ export const lagersEtFermentationsBassesBundle = createCollectionBundle({
   },
   assetsReady: true
 });
+
+export const lagersEtFermentationsBassesCollection = lagersEtFermentationsBassesBundleSource.collection;
+export const lagersEtFermentationsBassesCards = lagersEtFermentationsBassesBundleSource.cards;
+export const lagersEtFermentationsBassesCardsById = lagersEtFermentationsBassesBundleSource.cardsById;
+export const revealableLagersEtFermentationsBassesCards = lagersEtFermentationsBassesBundleSource.revealableCards;
+export const validateLagersEtFermentationsBassesCollection = lagersEtFermentationsBassesBundleSource.validate;
+export const lagersEtFermentationsBassesBundle = lagersEtFermentationsBassesBundleSource;
