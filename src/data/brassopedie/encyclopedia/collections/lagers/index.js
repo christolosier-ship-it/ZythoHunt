e9f66e9352b1ede4846a1d22bc23158ba0f7634f
Kristo <christo.losier@gmail.com>
2026-08-08 12:19:59 +0200

    refactor: compose les domaines sans ecrasement silencieux

--- src/data/brassopedie/encyclopedia/collections/lagers/index.js
+++ src/data/brassopedie/encyclopedia/collections/lagers/index.js
@@ -1,14 +1,15 @@
+import { composeEncyclopedicCards } from "../../compose-encyclopedic-cards.js";
 import { lagersEnrichmentPart1 as foundationsLightAndClassicPils } from "./foundations-light-and-classic-pils.js";
 import { pilsnersAndPaleSpecialties } from "./pilsners-and-pale-specialties.js";
 import { lagersEnrichmentPart3 as amberAndClassicDarkLagers } from "./amber-and-classic-dark-lagers.js";
 import { lagersEnrichmentPart4 as regionalDarkBocksAndNewWorldLagers } from "./regional-dark-bocks-and-new-world-lagers.js";
 
-const cards = Object.freeze({
-  ...foundationsLightAndClassicPils,
-  ...pilsnersAndPaleSpecialties,
-  ...amberAndClassicDarkLagers,
-  ...regionalDarkBocksAndNewWorldLagers
-});
+const cards = composeEncyclopedicCards(
+  foundationsLightAndClassicPils,
+  pilsnersAndPaleSpecialties,
+  amberAndClassicDarkLagers,
+  regionalDarkBocksAndNewWorldLagers
+);
 
 export const lagersEncyclopedicEnrichment = Object.freeze({
   version: "2026-08-architecture-v2",
