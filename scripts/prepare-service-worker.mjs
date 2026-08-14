48f458ddeef6222c3de0ccf7f772a8be3c4a415a
Kristo <christo.losier@gmail.com>
2026-08-14 12:10:18 +0200

    refactor(tasting): simplifie le précache après retrait de l'index sensoriel

--- scripts/prepare-service-worker.mjs
+++ scripts/prepare-service-worker.mjs
@@ -14,8 +14,7 @@ const urls = new Set([
   "./",
   "./offline.html",
   "./manifest.webmanifest",
-  "./beer-search-index.json",
-  "./beer-sensory-index.json"
+  "./beer-search-index.json"
 ]);
 
 for (const match of html.matchAll(/(?:src|href)=["']([^"']+)["']/g)) {
